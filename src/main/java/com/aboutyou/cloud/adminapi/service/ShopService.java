ffbc19261bcbde8df589d588f3b7805af150eda3
admin-api-gitlab-runner <team-operations+noreply@aboutyou.com>
2020-10-07 15:15:40 +0000

    add vendor extensions

---
+++ src/main/java/com/aboutyou/cloud/adminapi/service/ShopService.java
@@ -0,0 +1,28 @@
+package com.aboutyou.cloud.adminapi.service;
+
+import java.util.Map;
+
+import com.aboutyou.cloud.adminapi.exception.ApiErrorException;
+import com.aboutyou.cloud.adminapi.exception.ConnectionException;
+import com.aboutyou.cloud.adminapi.http.HttpClient;
+import com.aboutyou.cloud.adminapi.model.*;
+
+@SuppressWarnings("unchecked")
+public class ShopService extends AbstractService {
+    public ShopService(HttpClient httpClient) {
+        super(httpClient);
+    }
+
+    public Shop create(Shop model) throws ApiErrorException, ConnectionException {
+        Class<Shop> responseModel = (Class<Shop>)(Class<?>)Shop.class;
+
+        return this.request("post", this.resolvePath("/shops"), null, responseModel, model);
+    }
+
+    public Shop create(Shop model, ApiOptions options) throws ApiErrorException, ConnectionException {
+        Class<Shop> responseModel = (Class<Shop>)(Class<?>)Shop.class;
+
+        return this.request("post", this.resolvePath("/shops"), options, responseModel, model);
+    }
+
+}
