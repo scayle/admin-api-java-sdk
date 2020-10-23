5a96cec0f22858c10c3a004e83ec89d7f5f5ce28
admin-api-gitlab-runner <team-operations+noreply@aboutyou.com>
2020-10-23 09:22:34 +0000

    Merge branch 'feature/BCO-3226-delete-campaigns-campaignid' into 'master'
    Feature/bco 3226 delete campaigns campaignid
    
    See merge request aboutyou/cc-babo/admin-api!29

--- src/main/java/com/aboutyou/cloud/adminapi/service/CampaignService.java
+++ src/main/java/com/aboutyou/cloud/adminapi/service/CampaignService.java
@@ -37,4 +37,38 @@ public class CampaignService extends AbstractService {
         return this.requestCollection("get", this.resolvePath("/campaigns"), options, responseModel);
     }
 
+    public Campaign get(Integer campaignId) throws ApiErrorException, ConnectionException {
+        Class<Campaign> responseModel = (Class<Campaign>)(Class<?>)Campaign.class;
+
+        return this.request("get", this.resolvePath("/campaigns/%s", campaignId), null, responseModel);
+    }
+
+    public Campaign get(Integer campaignId, ApiOptions options) throws ApiErrorException, ConnectionException {
+        Class<Campaign> responseModel = (Class<Campaign>)(Class<?>)Campaign.class;
+
+        return this.request("get", this.resolvePath("/campaigns/%s", campaignId), options, responseModel);
+    }
+
+    public Campaign update(Integer campaignId, Campaign model) throws ApiErrorException, ConnectionException {
+        Class<Campaign> responseModel = (Class<Campaign>)(Class<?>)Campaign.class;
+
+        return this.request("put", this.resolvePath("/campaigns/%s", campaignId), null, responseModel, model);
+    }
+
+    public Campaign update(Integer campaignId, Campaign model, ApiOptions options) throws ApiErrorException, ConnectionException {
+        Class<Campaign> responseModel = (Class<Campaign>)(Class<?>)Campaign.class;
+
+        return this.request("put", this.resolvePath("/campaigns/%s", campaignId), options, responseModel, model);
+    }
+
+    public void delete(Integer campaignId) throws ApiErrorException, ConnectionException {
+
+        this.request("delete", this.resolvePath("/campaigns/%s", campaignId), null, null);
+    }
+
+    public void delete(Integer campaignId, ApiOptions options) throws ApiErrorException, ConnectionException {
+
+        this.request("delete", this.resolvePath("/campaigns/%s", campaignId), options, null);
+    }
+
 }
--- src/test/java/com/aboutyou/cloud/adminapi/CampaignTest.java
+++ src/test/java/com/aboutyou/cloud/adminapi/CampaignTest.java
@@ -47,4 +47,43 @@ public class CampaignTest extends BaseApiTest {
         }
     }
 
+    @Test
+    public void testGet() throws Exception {
+
+    Campaign responseEntity = this.api.campaigns().get(1,  null);
+
+        String expectedResponseJson = this.loadFixture("/fixtures/CampaignGetResponse.json");
+        assertThatJson(expectedResponseJson)
+            .when(TREATING_NULL_AS_ABSENT)
+            .isEqualTo(this.jsonSerializer.serializeApiObject(responseEntity));
+
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        String expectedRequestJson = this.loadFixture("/fixtures/CampaignUpdateRequest.json");
+        Campaign requestEntity = this.jsonSerializer.unserializeApiObject(expectedRequestJson, Campaign.class);
+
+        assertThatJson(expectedRequestJson)
+            .when(TREATING_NULL_AS_ABSENT)
+            .isEqualTo(this.jsonSerializer.serializeApiObject(requestEntity));
+
+    Campaign responseEntity = this.api.campaigns().update(1, requestEntity,  null);
+
+        String expectedResponseJson = this.loadFixture("/fixtures/CampaignUpdateResponse.json");
+        assertThatJson(expectedResponseJson)
+            .when(TREATING_NULL_AS_ABSENT)
+            .isEqualTo(this.jsonSerializer.serializeApiObject(responseEntity));
+
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    this.api.campaigns().delete(1,  null);
+
+    }
+
 }
