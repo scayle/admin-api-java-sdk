33c92a1747b97a1dac25c9a1135ed99f3fe01ed1
admin-api-gitlab-runner <team-operations+noreply@aboutyou.com>
2020-10-28 14:37:04 +0000

    Merge branch 'BCO-3230-post-campaigns-campaignid-reductions' into 'master'
    Bco 3230 post campaigns campaignid reductions
    
    See merge request aboutyou/cc-babo/admin-api!31

---
+++ src/main/java/com/aboutyou/cloud/adminapi/model/ProductVariantCampaignReduction.java
@@ -0,0 +1,32 @@
+package com.aboutyou.cloud.adminapi.model;
+
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.annotations.SerializedName;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductVariantCampaignReduction implements ApiObjectInterface {
+    /**
+    * Backbone Core's internal product variant identifier.
+    */
+    @SerializedName("productVariantId")
+    Integer productVariantId;
+
+    /**
+    * Tenant provided product variant identifier.
+    */
+    @SerializedName("productVariantReferenceKey")
+    String productVariantReferenceKey;
+
+    /**
+    * The reduction percentage applied to this specific variant.
+    */
+    @SerializedName("reduction")
+    Integer reduction;
+
+}
