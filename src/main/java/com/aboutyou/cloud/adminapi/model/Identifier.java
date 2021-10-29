a4b411e576c61652fbf7d78a6818a4da48d27dc0
admin-api-gitlab-runner <team-operations+noreply@aboutyou.com>
2021-10-29 12:17:32 +0000

    Merge branch 'task/BCO-4553-support-only-url-encoded-url-parameters' into 'master'
    bugfix/BCO-4553 -  converted special character to encoded characters
    
    See merge request aboutyou/cc-babo/admin-api!175

--- src/main/java/com/aboutyou/cloud/adminapi/model/Identifier.java
+++ src/main/java/com/aboutyou/cloud/adminapi/model/Identifier.java
@@ -1,5 +1,9 @@
 package com.aboutyou.cloud.adminapi.model;
 
+import java.net.URLEncoder;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
 public class Identifier {
     private String identifier;
 
@@ -11,8 +15,14 @@ public class Identifier {
         return new Identifier(Integer.toString(id));
     }
 
-    public static Identifier fromKey(String referenceKey) {
-        return new Identifier("key=" + referenceKey);
+    public static Identifier fromKey(String referenceKey) throws UnsupportedEncodingException {
+
+        return new Identifier(
+            URLEncoder.encode(
+                "key=" + referenceKey,
+                StandardCharsets.UTF_8.name()
+            )
+        );
     }
 
     public String toString() {
