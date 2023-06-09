8bc040e9b7d4addba8fda1eb1dd4a93437e33ebc
Ivan Babenko <ivan.babenko@aboutyou.com>
2023-06-09 12:02:31 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-6110' into 'master'
    [BCO-6110] Add additional order.cost fields
    
    Closes BCO-6110
    
    See merge request aboutyou/cc-babo/admin-api!325

--- src/main/java/com/aboutyou/cloud/adminapi/model/OrderCost.java
+++ src/main/java/com/aboutyou/cloud/adminapi/model/OrderCost.java
@@ -30,15 +30,33 @@ public class OrderCost implements ApiObjectInterface {
     OrderCostTax tax;
 
     /**
-    * 
+    * The price is calculated including taxes and all applicable reductions such as discounts for sale and campaigns (should a campaign key be provdided on the request).
     */
     @SerializedName("withTax")
     Integer withTax;
 
     /**
-    * 
+    * This price excludes taxes, but also includes all applicable reductions.
     */
     @SerializedName("withoutTax")
     Integer withoutTax;
+
+    /**
+    * The price is calculated including taxes and all applicable reductions such as discounts for sale and campaigns (should a campaign key be provdided on the request), and membership discount.
+    */
+    @SerializedName("withTaxWithMembershipDiscountWithoutServiceCosts")
+    Integer withTaxWithMembershipDiscountWithoutServiceCosts;
+
+    /**
+    * This price excludes taxes, but also includes all applicable reductions and membership discount.
+    */
+    @SerializedName("withoutTaxWithMembershipDiscount")
+    Integer withoutTaxWithMembershipDiscount;
+
+    /**
+    * The exact cost captured: order total value - giftcard value - total membership discount
+    */
+    @SerializedName("costCapture")
+    Integer costCapture;
 
 }
