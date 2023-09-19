280df74fb8527817707494b0e40e597677f1f8b3
Ivan Babenko <ivan.babenko@aboutyou.com>
2023-09-19 14:59:51 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-6264' into 'master'
    [BCO-6264] Add merchant return addresses and contacts
    
    Closes BCO-6264
    
    See merge request aboutyou/cc-babo/admin-api!344

--- src/test/java/com/aboutyou/cloud/adminapi/MerchantTest.java
+++ src/test/java/com/aboutyou/cloud/adminapi/MerchantTest.java
@@ -84,4 +84,176 @@ public class MerchantTest extends BaseApiTest {
 
     }
 
+    @Test
+    public void testAllContacts() throws Exception {
+
+        ApiOptions options = ApiOptions.builder().build();
+        ApiCollection<MerchantContact> responseEntity = this.api.merchants().allContacts(Identifier.fromId(1), options);
+
+        String expectedResponseJson = this.loadFixture("/fixtures/MerchantAllContactsResponse.json");
+        assertThatJson(expectedResponseJson)
+            .when(TREATING_NULL_AS_ABSENT)
+            .isEqualTo(this.jsonSerializer.serializeApiObject(responseEntity));
+
+
+        for (MerchantContact entity : responseEntity.getEntities()) {
+        }
+    }
+
+    @Test
+    public void testGetContact() throws Exception {
+
+        ApiOptions options = ApiOptions.builder().build();
+        MerchantContact responseEntity = this.api.merchants().getContact(Identifier.fromId(1), 1, options);
+
+        String expectedResponseJson = this.loadFixture("/fixtures/MerchantGetContactResponse.json");
+        assertThatJson(expectedResponseJson)
+            .when(TREATING_NULL_AS_ABSENT)
+            .isEqualTo(this.jsonSerializer.serializeApiObject(responseEntity));
+
+
+    }
+
+    @Test
+    public void testCreateContact() throws Exception {
+        String expectedRequestJson = this.loadFixture("/fixtures/MerchantCreateContactRequest.json");
+        MerchantContact requestEntity = this.jsonSerializer.unserializeApiObject(expectedRequestJson, MerchantContact.class);
+
+        assertThatJson(expectedRequestJson)
+            .when(TREATING_NULL_AS_ABSENT)
+            .isEqualTo(this.jsonSerializer.serializeApiObject(requestEntity));
+
+        ApiOptions options = ApiOptions.builder().build();
+        MerchantContact responseEntity = this.api.merchants().createContact(Identifier.fromId(1), requestEntity, options);
+
+        String expectedResponseJson = this.loadFixture("/fixtures/MerchantCreateContactResponse.json");
+        assertThatJson(expectedResponseJson)
+            .when(TREATING_NULL_AS_ABSENT)
+            .isEqualTo(this.jsonSerializer.serializeApiObject(responseEntity));
+
+
+    }
+
+    @Test
+    public void testUpdateContact() throws Exception {
+        String expectedRequestJson = this.loadFixture("/fixtures/MerchantUpdateContactRequest.json");
+        MerchantContact requestEntity = this.jsonSerializer.unserializeApiObject(expectedRequestJson, MerchantContact.class);
+
+        assertThatJson(expectedRequestJson)
+            .when(TREATING_NULL_AS_ABSENT)
+            .isEqualTo(this.jsonSerializer.serializeApiObject(requestEntity));
+
+        ApiOptions options = ApiOptions.builder().build();
+        MerchantContact responseEntity = this.api.merchants().updateContact(Identifier.fromId(1), 1, requestEntity, options);
+
+        String expectedResponseJson = this.loadFixture("/fixtures/MerchantUpdateContactResponse.json");
+        assertThatJson(expectedResponseJson)
+            .when(TREATING_NULL_AS_ABSENT)
+            .isEqualTo(this.jsonSerializer.serializeApiObject(responseEntity));
+
+
+    }
+
+    @Test
+    public void testDeleteContact() throws Exception {
+
+        ApiOptions options = ApiOptions.builder().build();
+        this.api.merchants().deleteContact(Identifier.fromId(1), 1, options);
+
+    }
+
+    @Test
+    public void testAllReturnAddresses() throws Exception {
+
+        ApiOptions options = ApiOptions.builder().build();
+        ApiCollection<MerchantReturnAddress> responseEntity = this.api.merchants().allReturnAddresses(Identifier.fromId(1), options);
+
+        String expectedResponseJson = this.loadFixture("/fixtures/MerchantAllReturnAddressesResponse.json");
+        assertThatJson(expectedResponseJson)
+            .when(TREATING_NULL_AS_ABSENT)
+            .isEqualTo(this.jsonSerializer.serializeApiObject(responseEntity));
+
+
+        for (MerchantReturnAddress entity : responseEntity.getEntities()) {
+        }
+    }
+
+    @Test
+    public void testGetReturnAddress() throws Exception {
+
+        ApiOptions options = ApiOptions.builder().build();
+        MerchantReturnAddress responseEntity = this.api.merchants().getReturnAddress(Identifier.fromId(1), 1, options);
+
+        String expectedResponseJson = this.loadFixture("/fixtures/MerchantGetReturnAddressResponse.json");
+        assertThatJson(expectedResponseJson)
+            .when(TREATING_NULL_AS_ABSENT)
+            .isEqualTo(this.jsonSerializer.serializeApiObject(responseEntity));
+
+
+    }
+
+    @Test
+    public void testCreateReturnAddress() throws Exception {
+        String expectedRequestJson = this.loadFixture("/fixtures/MerchantCreateReturnAddressRequest.json");
+        MerchantReturnAddress requestEntity = this.jsonSerializer.unserializeApiObject(expectedRequestJson, MerchantReturnAddress.class);
+
+        assertThatJson(expectedRequestJson)
+            .when(TREATING_NULL_AS_ABSENT)
+            .isEqualTo(this.jsonSerializer.serializeApiObject(requestEntity));
+
+        ApiOptions options = ApiOptions.builder().build();
+        MerchantReturnAddress responseEntity = this.api.merchants().createReturnAddress(Identifier.fromId(1), requestEntity, options);
+
+        String expectedResponseJson = this.loadFixture("/fixtures/MerchantCreateReturnAddressResponse.json");
+        assertThatJson(expectedResponseJson)
+            .when(TREATING_NULL_AS_ABSENT)
+            .isEqualTo(this.jsonSerializer.serializeApiObject(responseEntity));
+
+
+    }
+
+    @Test
+    public void testUpdateReturnAddress() throws Exception {
+        String expectedRequestJson = this.loadFixture("/fixtures/MerchantUpdateReturnAddressRequest.json");
+        MerchantReturnAddress requestEntity = this.jsonSerializer.unserializeApiObject(expectedRequestJson, MerchantReturnAddress.class);
+
+        assertThatJson(expectedRequestJson)
+            .when(TREATING_NULL_AS_ABSENT)
+            .isEqualTo(this.jsonSerializer.serializeApiObject(requestEntity));
+
+        ApiOptions options = ApiOptions.builder().build();
+        MerchantReturnAddress responseEntity = this.api.merchants().updateReturnAddress(Identifier.fromId(1), 1, requestEntity, options);
+
+        String expectedResponseJson = this.loadFixture("/fixtures/MerchantUpdateReturnAddressResponse.json");
+        assertThatJson(expectedResponseJson)
+            .when(TREATING_NULL_AS_ABSENT)
+            .isEqualTo(this.jsonSerializer.serializeApiObject(responseEntity));
+
+
+    }
+
+    @Test
+    public void testDeleteReturnAddress() throws Exception {
+
+        ApiOptions options = ApiOptions.builder().build();
+        this.api.merchants().deleteReturnAddress(Identifier.fromId(1), 1, options);
+
+    }
+
+    @Test
+    public void testAttachCarrier() throws Exception {
+
+        ApiOptions options = ApiOptions.builder().build();
+        this.api.merchants().attachCarrier(Identifier.fromId(1), Identifier.fromId(1), "acme", options);
+
+    }
+
+    @Test
+    public void testDetachCarrier() throws Exception {
+
+        ApiOptions options = ApiOptions.builder().build();
+        this.api.merchants().detachCarrier(Identifier.fromId(1), Identifier.fromId(1), "acme", options);
+
+    }
+
 }
