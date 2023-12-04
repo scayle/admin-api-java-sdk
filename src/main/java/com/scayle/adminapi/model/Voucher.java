ed3ec5d2d6c6b0e12eab4dcd9074890f9a040891
Zelimkhan Khalidov <zelimkhan.khalidov@aboutyou.com>
2023-12-04 15:51:41 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-6475' into 'master'
    BCO-6475 extend Voucher by "isApplicableToPromotions"
    
    Closes BCO-6475
    
    See merge request aboutyou/cc-babo/admin-api!365

--- src/main/java/com/scayle/adminapi/model/Voucher.java
+++ src/main/java/com/scayle/adminapi/model/Voucher.java
@@ -38,6 +38,12 @@ public class Voucher implements ApiObjectInterface {
     /**
     * 
     */
+    @SerializedName("isApplicableToPromotions")
+    Boolean isApplicableToPromotions;
+
+    /**
+    * 
+    */
     @SerializedName("name")
     String name;
 
