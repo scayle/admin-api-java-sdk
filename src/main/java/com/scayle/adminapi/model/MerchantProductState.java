dc2b6661fef267671e9b78eec9808af0930abd4a
Sagar Karmur <sagar.karmur@aboutyou.com>
2023-12-27 15:36:20 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-6488' into 'master'
    BCO-6488
    
    Closes BCO-6488
    
    See merge request aboutyou/cc-babo/admin-api!367

---
+++ src/main/java/com/scayle/adminapi/model/MerchantProductState.java
@@ -0,0 +1,26 @@
+package com.scayle.adminapi.model;
+
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.annotations.SerializedName;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MerchantProductState implements ApiObjectInterface {
+    /**
+    * Reference key of the merchant to which the product belongs to.
+    */
+    @SerializedName("merchantReferenceKey")
+    String merchantReferenceKey;
+
+    /**
+    * State of the merchant product.
+    */
+    @SerializedName("state")
+    String state;
+
+}
--- src/main/java/com/scayle/adminapi/model/ProductState.java
+++ src/main/java/com/scayle/adminapi/model/ProductState.java
@@ -12,9 +12,21 @@ import lombok.Setter;
 @Setter
 public class ProductState implements ApiObjectInterface {
     /**
-    * The state of the product determined by the state evaluation process. The only possible values to request are `live`, `draft` and `blocked`. The `problem` state can only be the result of the state evaluation process. If product is in problem state, the reasons are listed in read-only 'problems' field. The `new` and `inApproval` states can be set in the SCAYLE Panel.
+    * The state of the product determined by the state evaluation process. The only possible values to request are `live`, `draft` and `blocked`. The `problem` state can only be the result of the state evaluation process. If product is in problem state, the reasons are listed in read-only 'problems' field. The `new` and `inApproval` states can be set in the SCAYLE Panel. If a product belongs to multiple merchants, the state is returned based on the hierarchical order live, inApproval, problem, blocked, draft
     */
     @SerializedName("state")
     String state = "live";
+
+    /**
+    * A list of merchant reference keys the merchant product belongs to.
+    */
+    @SerializedName("merchantReferenceKeys")
+    List<String> merchantReferenceKeys;
+
+    /**
+    * A list of merchant keys to which the merchant product belongs to and the state of the merchant product.
+    */
+    @SerializedName("merchantProductStates")
+    List<MerchantProductState> merchantProductStates;
 
 }
