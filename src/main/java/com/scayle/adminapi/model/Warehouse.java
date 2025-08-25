f6e4abd0353f4d44eb42160b0a9661a49cb22d6e
Sagar Karmur <sagar.karmur@aboutyou.com>
2025-08-25 15:29:33 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-8098' into 'master'
    Resolve BCO-8098
    
    Closes BCO-8098
    
    See merge request aboutyou/cc-babo/admin-api!559

--- src/main/java/com/scayle/adminapi/model/Warehouse.java
+++ src/main/java/com/scayle/adminapi/model/Warehouse.java
@@ -25,6 +25,13 @@ public class Warehouse extends AbstractModel  {
     String referenceKey;
 
     /**
+    * This flag should be set to true only when warehouse is a Zeos warehouse. It is not allowed to update this field after creation of the warehouse
+
+    */
+    @SerializedName("isZeosWarehouse")
+    Boolean isZeosWarehouse;
+
+    /**
     * A list of merchants the warehouse is attached to.
     */
     @SerializedName("merchants")
