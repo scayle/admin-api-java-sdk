12ec19105b84ae2d04362c2d0edaf65d2b3b6c97
Matthias Reichenbecher <matthias.reichenbecher@scayle.com>
2026-06-26 07:02:45 +0000

    [AdminAPI-dev-master] Merge branch 'task/BCO-8814' into 'master'
    BCO-8814
    
    Closes BCO-8814
    
    See merge request aboutyou/cc-babo/admin-api!655

--- src/main/java/com/scayle/adminapi/model/OrderFee.java
+++ src/main/java/com/scayle/adminapi/model/OrderFee.java
@@ -19,7 +19,15 @@ public class OrderFee extends AbstractModel  {
     OrderFeeAmount amount;
 
     /**
-    * Valid values in case of non-external order: [ payment, delivery, percentage_payment, return, additional]
+    * Categorises the fee. Valid values in case of non-external orders:
+  - `payment`
+  - `percentage_payment`
+  - `delivery`
+  - `shipment`
+  - `return`
+  - `additional`
+  - `conditional`
+
 If the order has an external price, this field becomes optional and can contain any string value defined by the client.
 
     */
@@ -27,7 +35,20 @@ If the order has an external price, this field becomes optional and can contain 
     String category;
 
     /**
-    * If the order has an external price, this field becomes optional and can contain any string value defined by the client.
+    * Identifies the specific fee within its `category`. Valid values in case of non-external orders:
+  - `base_payment_service_cost`
+  - `deferred_payment_service_cost`
+  - `instalment_payment_service_cost`
+  - `custom_payment_service_cost`
+  - `base_shipping_service_cost`
+  - `express_shipping_service_cost`
+  - `yearly_interest_rate`
+  - `return_costs`
+  - `not_refundable_cost`
+  - `home_delivery_service_fee`
+
+If the order has an external price, this field becomes optional and can contain any string value defined by the client.
+
     */
     @SerializedName("key")
     String key;
